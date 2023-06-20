980e14ef94e5a6b352b0926adeafdce0fbb418a6
Raphael Torquato <raphael.martini@live.com>
2023-06-19 22:35:49 -0300

    update to mutable value
    update to mutable value

--- Sources/BudzSharedDTO/RegisterResponseDTO.swift
+++ Sources/BudzSharedDTO/RegisterResponseDTO.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct RegisterResponseDTO: Codable {
     public let error: Bool
-    public let reason: String? = nil
+    public var reason: String? = nil
     
     public init(error: Bool, reason: String? = nil) {
         self.error = error
